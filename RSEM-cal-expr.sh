39051e7615035f7626edfb065987925fcec37988
Khushbu Patel <kpatel427@gatech.edu>
2021-08-17 14:56:11 -0400

    update

--- RSEM-cal-expr.sh
+++ RSEM-cal-expr.sh
@@ -9,20 +9,20 @@
 
 
 #echo "Generating indexes..."
-#/home/patelk26/KP/tools/RSEM/rsem-prepare-reference --gtf /home/patelk26/KP/hg19/Homo_sapiens.GRCh38.86.gtf --bowtie2 --bowtie2-path /mnt/isilon/maris_lab/target_nbl_ngs/shared_resources/apps/bowtie2/bowtie2-2.2.8/ /home/patelk26/KP/hg19/Homo_sapiens.GRCh38.dna.primary_assembly.fa /home/patelk26/KP/papThyCarc_bulkRNASeq/bowtie_2.2.8_hg38_index/GRCh38
+#/home/patelk26/KP/tools/RSEM/rsem-prepare-reference --gtf /home/patelk26/KP/hg19/Homo_sapiens.GRCh38.86.gtf --bowtie2 --bowtie2-path ~/apps/bowtie2/bowtie2-2.2.8/ /home/patelk26/KP/hg19/Homo_sapiens.GRCh38.dna.primary_assembly.fa /home/patelk26/KP/papThyCarc_bulkRNASeq/bowtie_2.2.8_hg38_index/GRCh38
 
-#/mnt/isilon/maris_lab/target_nbl_ngs/shared_resources/apps/bowtie2/bowtie2-2.2.8/bowtie2-build -f home/patelk26/KP/hg19/Homo_sapiens.GRCh38.dna.primary_assembly.fa /home/patelk26/KP/papThyCarc_bulkRNASeq/bowtie_2.2.8_hg38_index/GRCh38
+#~/apps/bowtie2/bowtie2-2.2.8/bowtie2-build -f home/patelk26/KP/hg19/Homo_sapiens.GRCh38.dna.primary_assembly.fa /home/patelk26/KP/papThyCarc_bulkRNASeq/bowtie_2.2.8_hg38_index/GRCh38
 
 
 echo "RSEM calculate expression..."
 /home/patelk26/KP/tools/RSEM/rsem-calculate-expression --bowtie2 \
-                           --bowtie2-path /mnt/isilon/maris_lab/target_nbl_ngs/shared_resources/apps/bowtie2/bowtie2-2.2.8/ \
+                           --bowtie2-path ~/apps/bowtie2/bowtie2-2.2.8/ \
                            --paired-end \
                            -p 16 \
-                           /mnt/isilon/maris_lab/target_nbl_ngs/papThyCarcinoma_bulkRNASeq/6525844154_ds.790c25ced1574bbb86c1166a4fa1d902/6525844154_S1_L002_R1_001.fastq.gz \
-                           /mnt/isilon/maris_lab/target_nbl_ngs/papThyCarcinoma_bulkRNASeq/6525844154_ds.790c25ced1574bbb86c1166a4fa1d902/6525844154_S1_L002_R2_001.fastq.gz \
+                           ~/papThyCarcinoma_bulkRNASeq/sample1_R1_001.fastq.gz \
+                           ~/papThyCarcinoma_bulkRNASeq/sample1_R2_001.fastq.gz \
                            /home/patelk26/KP/papThyCarc_bulkRNASeq/bowtie_2.2.8_hg38_index/GRCh38 \
-                           6525844154_S1_L002
+                           sample1
 
 
 echo "Done!"
